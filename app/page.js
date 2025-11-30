1cded4a1a5092cc551d490dca3e210bd6a777c7e
yward77 <yazanward8@gmail.com>
2025-11-30 21:42:59 +0200

    update

--- app/page.js
+++ app/page.js
@@ -6,10 +6,6 @@ import Footer from "./Footer/Footer";
 export default function Home() {
   return (
     <>
-    <head>
-      <title>Home Page</title>
-      <meta name="description" content="Welcome to our home page!" />
-    </head>
     <link rel="icon" href="/favicon.ico" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
